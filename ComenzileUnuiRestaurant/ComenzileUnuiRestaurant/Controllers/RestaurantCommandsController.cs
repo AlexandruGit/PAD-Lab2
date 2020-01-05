8ed16b9b06c15195d4849c0c61a8d405bf6d25da
AlexandruGit <44066971+AlexandruGit@users.noreply.github.com>
2020-01-05 21:48:03 +0200

    Added a CRUD proj
    Added a project with simple CRUD for restaurant orders

---
+++ ComenzileUnuiRestaurant/ComenzileUnuiRestaurant/Controllers/RestaurantCommandsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ComenzileUnuiRestaurant.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class RestaurantCommandsController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<RestaurantCommandsController> _logger;
+
+		public RestaurantCommandsController(ILogger<RestaurantCommandsController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<RestaurantCommand> Get()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new RestaurantCommand
+			{
+			
+			})
+			.ToArray();
+		}
+	}
+}
---
+++ ComenzileUnuiRestaurant/ComenzileUnuiRestaurant/RestaurantCommand.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace ComenzileUnuiRestaurant
+{
+	public class RestaurantCommand
+	{
+		public List<string> Products { get; set; }
+		public decimal Price { get; set; }
+	}
+}
